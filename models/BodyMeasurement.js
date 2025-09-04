915847523fef344b15283407185b65141e63e359
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-04 18:41:14 +0200

    BodyMeasurement model

---
+++ models/BodyMeasurement.js
@@ -0,0 +1,50 @@
+import db from "../db.js";
+import { DataTypes } from "sequelize";
+
+const BodyMeasurement = db.define(
+    'BodyMeasurement', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        weight: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        arm: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        forearm: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        chest: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        waist: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        hips: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        thigh: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+        calf: {
+            type: DataTypes.FLOAT,
+            allowNull: true,            
+        },
+    }, {
+        tableName: 'body_measurements',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: false,
+    }
+);
+
+export default BodyMeasurement;
