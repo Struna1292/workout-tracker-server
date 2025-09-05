d14f917a83753be6acf7394e0506b1c0289cbba7
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-05 21:21:17 +0200

    workout template model definition

---
+++ models/WorkoutTemplate.js
@@ -0,0 +1,26 @@
+import db from "../db.js";
+import { DataTypes } from "sequelize";
+
+const WorkoutTemplate = db.define(
+    'WorkoutTemplate', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    }, {
+        tableName: 'workout_templates',
+        timestamps: false,
+    } 
+);
+
+export default WorkoutTemplate;
