f691fea0b8da22d229a49dc390a7a61d4b69e4fe
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-05 19:13:25 +0200

    Added body measurement update remove and get functionality

--- controllers/bodyMeasurementsController.js
+++ controllers/bodyMeasurementsController.js
@@ -1,5 +1,34 @@
 import User from "../models/User.js";
 
+export const userMeasurements = async (req, res, next) => {
+    try {
+        const user = await User.findByPk(req.user.id);
+
+        if (!user) {
+            console.log("Failed to load user measurements. User does not exist");
+            const err = new Error("Failed to load user measurements. User does not exist");
+            err.status = 404;
+            return next(err);
+        }
+        
+        const measurements = await user.getBodyMeasurements();
+
+        if (measurements.length == 0) {
+            const err = new Error("User has no measurements");
+            err.status = 404;
+            return next(err);
+        }
+
+        return res.status(200).json(measurements);
+    }
+    catch (error) {
+        console.log(`Error while loading user measurements from database: ${error}`);
+        const err = new Error("Internal server error while loading user measurements");
+        err.status = 500;
+        return next(err);
+    }
+};
+
 const validateNewMeasure = (reqBody, errors) => {
     if (!reqBody) {
         return undefined;
@@ -42,7 +71,7 @@ const validateNewMeasure = (reqBody, errors) => {
 };
 
 
-export const addMeasure = async (req, res, next) => {
+export const addMeasurement = async (req, res, next) => {
     try {
         const errors = [];
 
@@ -75,6 +104,95 @@ export const addMeasure = async (req, res, next) => {
     catch (error) {
         console.log(`Error while trying to add new measurement: ${error}`);
         const err = new Error("Internal server error while trying to add new measurement");
+        err.status = 500;
+        return next(err);
+    }
+};
+
+export const updateMeasurement = async (req, res, next) => {
+    try {
+        const measurementId = req.params.id;
+
+        const user = await User.findByPk(req.user.id);
+
+        if (!user) {
+            console.log("Failed to update user measurement. User does not exist");
+            const err = new Error("Failed to update user measurement. User does not exist");
+            err.status = 404;
+            return next(err);
+        }
+
+        const measurement = (await user.getBodyMeasurements({
+            where: { id: measurementId },
+            limit: 1
+        }))[0];        
+        
+        if (!measurement) {
+            console.log("Failed to update measurement. Measurement does not exist");
+            const err = new Error("Failed to update measurement. Measurement does not exist");
+            err.status = 404;
+            return next(err);            
+        }
+
+        const errors = [];
+
+        const data = validateNewMeasure(req.body, errors);
+
+        if (!data) {
+            console.log("Failed to update measurement there is no valid data");
+            let message = "Cant update user measurement without any valid data.";
+            for (const error of errors) {
+                message += "\n" + error;
+            }   
+            const err = new Error(message);
+            err.status = 422;
+            return next(err);
+        }
+
+        await measurement.update(data);
+
+        return res.status(200).json({ message: "Successfully updated measurement" });
+    }
+    catch (error) {
+        console.log(`Error while trying to update measurement: ${error}`);
+        const err = new Error("Internal server error while updating measurement");
+        err.status = 500;
+        return next(err);
+    }
+};
+
+export const removeMeasurement = async (req, res, next) => {
+    try {
+        const measurementId = req.params.id;
+
+        const user = await User.findByPk(req.user.id);
+
+        if (!user) {
+            console.log("Failed to remove measurement. User does not exist");
+            const err = new Error("Failed to remove measurement. User does not exist");
+            err.status = 404;
+            return next(err);
+        }
+        
+        const measurement = (await user.getBodyMeasurements({
+            where: { id: measurementId },
+            limit: 1
+        }))[0];
+
+        if (!measurement) {
+            console.log("Failed to remove measurement. Measurement does not exist");
+            const err = new Error("Failed to remove measurement. Measurement does not exist");
+            err.status = 404;
+            return next(err);            
+        }
+
+        await measurement.destroy();
+
+        return res.status(204);
+    }
+    catch (error) {
+        console.log(`Error while trying to remove measurement by id: ${error}`);
+        const err = new Error("Internal server error while trying to remove measurement");
         err.status = 500;
         return next(err);
     }
--- routes/bodyMeasurementsRoutes.js
+++ routes/bodyMeasurementsRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import authToken from "../middlewares/authToken.js";
-import { addMeasure } from "../controllers/bodyMeasurementsController.js";
+import { userMeasurements, addMeasurement, updateMeasurement, removeMeasurement } from "../controllers/bodyMeasurementsController.js";
 
 const router = express.Router();
 
+// get user measurements
+router.get("", authToken, userMeasurements);
+
 // add new measure for logged in user
-router.post("", authToken, addMeasure);
+router.post("", authToken, addMeasurement);
+
+// update measure
+router.put("/:id", authToken, updateMeasurement);
+
+// remove measure
+router.delete("/:id", authToken, removeMeasurement);
 
 export default router;
