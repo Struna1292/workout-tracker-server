b078cfb9c4f41f459aa75e407d54b789e52c512c
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-06 12:43:11 +0200

    Added new models imports

--- sync.js
+++ sync.js
@@ -2,6 +2,8 @@ import db from "./db.js";
 import "./models/User.js";
 import "./models/BodyMeasurement.js";
 import "./models/associations.js";
+import "./models/WorkoutTemplate.js";
+import "./models/Exercise.js";
 
 try {
     await db.sync({ alter: true});
