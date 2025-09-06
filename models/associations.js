dc483c786afe709fb8e3f313e7860d451851dfb3
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-06 13:44:23 +0200

    Added relations between User and his Workout templates

--- models/associations.js
+++ models/associations.js
@@ -1,5 +1,13 @@
 import User from "./User.js";
 import BodyMeasurement from "./BodyMeasurement.js";
+import WorkoutTemplate from "./WorkoutTemplate.js";
 
+// relations between user and his body measurements
 User.hasMany(BodyMeasurement, { foreignKey: 'user_id', as: 'BodyMeasurements', onDelete: 'CASCADE'});
 BodyMeasurement.belongsTo(User, { foreignKey: 'user_id', as: 'User', onDelete: 'NO ACTION'});
+
+// relations between user and his workout templates
+User.hasMany(WorkoutTemplate, { foreignKey: 'user_id', as: 'WorkoutTemplates', onDelete: 'CASCADE'});
+WorkoutTemplate.belongsTo(User, { foreignKey: 'user_id', as: 'User', onDelete: 'NO ACTION'});
+
+// relations between workout template and exercises it includes
