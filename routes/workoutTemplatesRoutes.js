10db0c19e1a867dbe1179bf27aebf7502dd54823
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-06 18:20:14 +0200

    Basic workout templates routes

--- routes/workoutTemplatesRoutes.js
+++ routes/workoutTemplatesRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
 import authToken from "../middlewares/authToken.js";
+import { addWorkoutTemplate, removeWorkoutTemplate, userWorkoutTemplates, updateWorkoutTemplate } from "../controllers/workoutTemplatesController.js";
 
 const router = express.Router();
 
+// get user templates
+router.get("", authToken, userWorkoutTemplates);
 
+// add new user template
+router.post("", authToken, addWorkoutTemplate);
+
+// edit template
+router.put("", authToken, updateWorkoutTemplate);
+
+// remove user template
+router.delete("/:id", authToken, removeWorkoutTemplate);
 
 export default router;
