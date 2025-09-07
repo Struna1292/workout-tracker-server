d95e7ffddceb2f7fd139d574ad11b722ff10ab9d
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-07 22:03:02 +0200

    Added simple adding and getting user defined exercises

---
+++ controllers/exercisesController.js
@@ -0,0 +1,35 @@
+export const getUserExercises = async (req, res, next) => {
+    try {
+
+        const user = req.user;
+
+        const exercises = await user.getExercises();
+
+        res.status(200).json(exercises);
+    }
+    catch (error) {
+        console.log(`Error while trying to get user exercises: ${error}`);
+        const err = new Error("Internal server error while trying to get user exercises");
+        err.status = 500;
+        return next(err);
+    }
+};
+
+export const addUserExercise = async (req, res, next) => {
+    try {
+
+        const data = req.body;
+
+        const user = req.user;
+
+        await user.createExercise({ name: data.name });
+        
+        return res.status(200).json({ message: "Successfully added exercise" });
+    }
+    catch (error) {
+        console.log(error);
+        const err = new Error("");
+        err.status = 500;
+        return next(err);
+    }
+};
--- routes/exercisesRoutes.js
+++ routes/exercisesRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
 import authToken from "../middlewares/authToken.js";
+import loadUser from "../middlewares/loadUser.js";
+import checkBody from "../middlewares/checkBody.js";
+import { getUserExercises, addUserExercise } from "../controllers/exercisesController.js";
+
 
 const router = express.Router();
 
+// get user exercises
+router.get("", [authToken, loadUser], getUserExercises);
 
+// get global exercises
+
+// add user exercise
+router.post("", [checkBody, authToken, loadUser], addUserExercise);
 
 export default router;
