c3d33cdff3a859cdddeb3458aff9e52c295b10a4
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-07 21:31:04 +0200

    Added middleware to check if there is data in request body

---
+++ middlewares/checkBody.js
@@ -0,0 +1,14 @@
+// checks if there is data sent
+const checkBody = async (req, res, next) => {
+    if (!req.body) {
+        console.log('There is no data sent');
+        const err = new Error('There is no data sent');
+        err.status = 400;
+        return next(err);
+    }
+    else {
+        return next();
+    }
+};
+
+export default checkBody;
