c9b9380bc76fa32a74d24744702e2d0ff8d5fe6e
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-09 02:08:39 +0200

    Imported and added relations between Exercises and Muscle groups

--- models/associations.js
+++ models/associations.js
@@ -3,6 +3,8 @@ import BodyMeasurement from './BodyMeasurement.js';
 import WorkoutTemplate from './WorkoutTemplate.js';
 import Exercise from './Exercise.js';
 import WorkoutTemplateExercise from './WorkoutTemplateExercise.js';
+import MuscleGroup from './MuscleGroup.js';
+import ExerciseMuscleGroup from './ExerciseMuscleGroup.js';
 
 // relations between user and his body measurements
 User.hasMany(BodyMeasurement, { foreignKey: 'user_id', as: 'BodyMeasurements', onDelete: 'CASCADE'});
@@ -19,3 +21,7 @@ Exercise.belongsTo(User, { foreignKey: 'user_id', as: 'User', onDelete: 'NO ACTI
 // relations between workout template and exercises it includes
 Exercise.belongsToMany(WorkoutTemplate, { through: {model: WorkoutTemplateExercise, unique: false}, foreignKey: 'exercise_id', otherKey: 'workout_template_id'});
 WorkoutTemplate.belongsToMany(Exercise, { through: {model: WorkoutTemplateExercise, unique: false}, foreignKey: 'workout_template_id', otherKey: 'exercise_id'});
+
+// relations between exercise and muscle groups it works
+Exercise.belongsToMany(MuscleGroup, { through: ExerciseMuscleGroup, foreignKey: 'exercise_id', otherKey: 'muscle_group_id'});
+MuscleGroup.belongsToMany(Exercise, { through: ExerciseMuscleGroup, foreignKey: 'muscle_group_id', otherKey: 'exercise_id'});
