ed8fc1ac08b8a86faa9e480a3988996bae9b5df6
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-12 19:49:49 +0200

    Added getting editing and removing workouts

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -3,7 +3,53 @@ import WorkoutExercise from '../models/WorkoutExercise.js';
 import Set from '../models/Set.js';
 
 export const getUserWorkouts = async (req, res, next) => {
+    try {
+        const user = req.user;
 
+        const workoutsData = [];
+
+        const workouts = await user.getWorkouts();
+        for (const workout of workouts) {
+
+            const template = await workout.getTemplate();
+
+            const currWorkout = {
+                id: workout.id,
+                template: template ? template.name : null,
+                duration: workout.duration,
+                date: workout.date,
+                exercises: []
+            };
+
+            const workoutExercises = await workout.getExercises();
+            for (const exercise of workoutExercises) {
+
+                const currExercise = {
+                    id: exercise.id,
+                    name: exercise.name,
+                    position: exercise.WorkoutExercise.position,
+                    sets: [],
+                };
+
+                const sets = await exercise.WorkoutExercise.getSets();
+                for (const set of sets) {
+                    currExercise.sets.push({ reps: set.reps, weight: set.weight });
+                }
+
+                currWorkout.exercises.push(currExercise);
+            }
+
+            workoutsData.push(currWorkout);
+        }
+
+        return res.status(200).json(workoutsData);
+    }   
+    catch (error) {
+        console.log(`Error while trying to load user workouts: ${error}`);
+        const err = new Error('Internal server error while trying to load workouts');
+        err.status = 500;
+        return next(err);
+    }
 };
 
 // maps global and user exercises where key is name, value is exercise object
@@ -128,9 +174,136 @@ export const addWorkout = async (req, res, next) => {
 };
 
 export const editWorkout = async (req, res, next) => {
+    try {
 
+        const workoutId = req.params.id;
+        const user = req.user;
+
+        const workout = (await user.getWorkouts({ where: {id: workoutId } }))[0];
+
+        if (!workout) {
+            console.log('Workout does not exist');
+            const err = new Error('Workout does not exist');
+            err.status = 404;
+            return next(err);
+        }        
+
+        const data = req.body;
+
+        if (data.templateName) {
+            const name = data.templateName.toLowerCase();
+
+            const templates = await user.getWorkoutTemplates();
+
+            for (const template of templates) {
+                if (template.name.toLowerCase() == name) {
+                    workout.workout_template_id = template.id;
+                    break;
+                }
+            }
+        }
+
+        const duration = data.duration;
+        if (duration && duration >= 0 && duration <= 1000000) {
+            workout.duration = Math.round(duration);
+        }
+
+        const date = new Date(data.date);
+        if (date != 'Invalid Date') {
+            workout.date = date;
+        }        
+
+        const exercises = data.exercises;
+        if (exercises) {
+
+            // clear previous exercises
+            await workout.setExercises([]);
+
+            // first map user exercises and global exercises
+            const map = await mapExercises(user);
+
+            // iterate through given exercises 
+            let exercisePosition = 0;
+            for (const exercise of exercises) {
+
+                if (!exercise.name) {
+                    continue;
+                }
+                const name = exercise.name.toLowerCase();
+
+                // check if exercise exists
+                if (!map.has(name)) {
+                    continue;
+                }
+                
+                const currExercise = map.get(name);
+
+                const currWorkoutExercise = await WorkoutExercise.create({
+                    workout_id: workout.id, 
+                    exercise_id: currExercise.id, 
+                    position: exercisePosition
+                });
+                exercisePosition++;
+
+                if (!exercise.sets) {
+                    continue;
+                }
+
+                const formatedSets = [];
+                let setPosition = 0;
+                for (const set of exercise.sets) {
+                    if (!set.reps || !set.weight || set.reps <= 0 || set.weight < 0) {
+                        continue;
+                    }
+                    formatedSets.push({
+                        workout_exercise_id: currWorkoutExercise.id, 
+                        reps: set.reps, 
+                        weight: set.weight, 
+                        position: setPosition 
+                    });
+                    setPosition++;
+                }
+
+                await Set.bulkCreate(formatedSets);
+            }
+        }        
+
+        await workout.save();
+
+        return res.status(200).json({ message: 'Successfully edited workout' });
+    }
+    catch (error) {
+        console.log(`Error while trying to edit workout: ${error}`);
+        const err = new Error('Internal server error while editing workout');
+        err.status = 500;
+        return next(err);
+    }
 };
 
 export const removeWorkout = async (req, res, next) => {
+    try {
 
+        const workoutId = req.params.id;
+        const user = req.user;
+
+        const workout = (await user.getWorkouts({ where: {id: workoutId } }))[0];
+
+        if (!workout) {
+            console.log('Workout does not exist');
+            const err = new Error('Workout does not exist');
+            err.status = 404;
+            return next(err);
+        }
+
+        await workout.destroy();
+        console.log('Successfully removed workout');
+        
+        return res.status(200).json({ message: 'Successfully removed workout' });
+    }   
+    catch (error) {
+        console.log(`Error while trying to remove workout`);
+        const err = new Error('Internal serve error while trying to remove workout');
+        err.status = 500;
+        return next(err);
+    }
 };
