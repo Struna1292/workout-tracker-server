73f0fb9c4fbcc0a820e60acdbdbdd11f73763788
Kacper <kacperkonradkowalczyk@gmail.com>
2025-09-24 14:55:00 +0200

    Added removing e-mail

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -48,3 +48,53 @@ export const changePassword = async (req, res, next) => {
         return next(err);
     }
 };
+
+export const removeEmail = async (req, res, next) => {
+    try {
+        const user = req.user;
+
+        // check if user has email
+        if (!user.email) {  
+            console.log('User has no e-mail');
+            const err = new Error('User has no e-mail');
+            err.status = 400;
+            return next(err);
+        }
+
+        // no password needed to remove not verified email
+        if (!user.email_verified) {
+            console.log('Successfully removed e-mail'); 
+            user.email = null;
+            await user.save();
+            return res.status(200).json({ message: 'Successfully removed e-mail' });
+        }
+
+        if (!req.body || !req.body.password) {
+            console.log('No password provided');
+            const err = new Error('No password provided');
+            err.status = 400;
+            return next(err);
+        }
+
+        const { password } = req.body;
+
+        if (!(await bcrypt.compare(password, user.password))) {
+            console.log('Wrong password');
+            const err = new Error('Wrong password');
+            err.status = 400;
+            return next(err);
+        }
+
+        user.email = null;
+        user.email_verified = false;
+        await user.save();
+
+        return res.status(200).json({ message: 'Successfully removed e-mail' });
+    }
+    catch (error) {
+        console.log(`Error while trying to remove e-mail: ${error}`);
+        const err = new Error('Internal server error while trying to remove user e-mail');
+        err.status = 500;
+        return next(err);
+    }
+};
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,12 +3,16 @@ import checkBody from '../middlewares/checkBody.js';
 import authToken from '../middlewares/authToken.js';
 import loadUser from '../middlewares/loadUser.js';
 import {
-    changePassword, 
+    changePassword,
+    removeEmail,
 } from '../controllers/usersController.js';
 
 const router = express.Router();
 
 // change user password 
-router.patch('/change-password', [checkBody, authToken, loadUser], changePassword);
+router.patch('/me/password', [checkBody, authToken, loadUser], changePassword);
+
+// remove user e-mail
+router.delete('/me/email', [authToken, loadUser], removeEmail);
 
 export default router;
