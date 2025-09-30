926c45ab7f013737aa413122b9b0e1baeca0d898
Kacper <kacperkonradkowalczyk@gmail.com>
2025-10-01 00:10:19 +0200

    Added addExercise function

--- models/Workout.js
+++ models/Workout.js
@@ -1,5 +1,6 @@
 import db from '../db.js';
 import { DataTypes } from 'sequelize';
+import WorkoutExercise from './WorkoutExercise.js';
 
 const Workout = db.define(
     'Workout', {
@@ -24,12 +25,35 @@ const Workout = db.define(
         date: {
             type: DataTypes.DATE,
             allowNull: false,
-            defaultValue: DataTypes.NOW,
         },
     }, {
         tableName: 'workouts',
         timestamps: false,
     } 
 );
+
+// default addExercise functions works on findOrCreate
+// instead of adding same exercise with different position 
+// it will override position
+Workout.prototype.addExercise = async function (exerciseId, position, sets) {
+
+    const workoutExercise = await WorkoutExercise.create({
+        workout_id: this.id,
+        exercise_id: exerciseId,
+        position: position,
+    });
+
+    if (sets) {
+        for (let i = 0; i < sets.length; i++) {
+            await workoutExercise.createSet({
+                reps: sets[i].reps,
+                weight: sets[i].weight,
+                position: i,
+            });
+        }
+    }
+
+    return workoutExercise;
+};
 
 export default Workout;
