a013afc48da8803253dad3e186dcf989c7843e21
Kacper <kacperkonradkowalczyk@gmail.com>
2025-10-03 19:55:35 +0200

    Added add email and change username functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,11 @@
 import bcrypt from 'bcrypt';
-import { validatePassword } from '../validations/userValidations.js';
+import User from '../models/User.js';
+import { 
+    validatePassword,
+    validateEmail,
+    validateUsername,
+} from '../validations/userValidations.js';
+import sequelize from '../db.js';
 
 export const changePassword = async (req, res, next) => {
     try {
@@ -104,6 +110,106 @@ export const removeEmail = async (req, res, next) => {
     catch (error) {
         console.log(`Error while trying to remove e-mail: ${error}`);
         const err = new Error('Internal server error while trying to remove user e-mail');
+        err.status = 500;
+        return next(err);
+    }
+};
+
+export const addEmail = async (req, res, next) => {
+    try {
+
+        const { email } = req.body;
+        const user = req.user;
+
+        if (user.email != null) {
+            console.log('User already has email');
+            const err = new Error('User already has email');
+            err.status = 400;
+            return next(err);
+        }
+
+        const errors = [];
+
+        validateEmail(email, errors);
+
+        if (errors.length > 0) {
+            console.log('Failed to add email');
+            const err = new Error('Failed to add email');
+            err.status = 422;
+            err.details = errors;
+            return next(err);
+        }
+
+        user.email = email;
+        await user.save();
+
+        return res.status(200).json({ message: 'Successfully added email' });
+    }
+    catch (error) {
+        console.log(`Error while trying to add email: ${error}`);
+        const err = new Error('Internal server error while trying to add email');
+        err.status = 500;
+        return next(err);
+    }
+};
+
+export const changeUsername = async (req, res, next) => {
+    try {
+
+        const { username } = req.body;
+        const user = req.user;
+
+        const errors = [];
+
+        validateUsername(username, errors);
+
+        if (errors.length > 0) {
+            console.log('Failed to change username');
+            const err = new Error('Failed to change username');
+            err.status = 422;
+            err.details = errors;
+            return next(err);
+        }
+        
+        // check if given username is the same
+        if (user.username == username) {
+            console.log('Username is the same');
+            const err = new Error('Given username is the same');
+            err.status = 400;
+            return next(err);
+        }
+
+        // check if given username is the same in lowercase as current
+        if (user.username.toLowerCase() == username.toLowerCase()) {
+
+            user.username = username;
+
+            await user.save();
+
+            return res.status(200).json({ message: 'Successfully changed username' });
+        }
+
+        const differentUser = await User.findOne({ where: sequelize.where(
+            sequelize.fn('LOWER', sequelize.col('username')),
+            username.toLowerCase() 
+        )});
+
+        if (differentUser) {
+            console.log('Username taken');
+            const err = new Error('This username is already taken');
+            err.status = 400;
+            return next(err);
+        }
+
+        user.username = username;
+
+        await user.save();
+
+        return res.status(200).json({ message: 'Successfully changed username' });
+    }
+    catch (error) {
+        console.log(`Error while trying to change username: ${error}`);
+        const err = new Error('Internal server error while trying to change username');
         err.status = 500;
         return next(err);
     }
