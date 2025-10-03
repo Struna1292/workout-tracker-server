3ada5ba9dac4127ae5c3f340297d2d24e2c449bf
Kacper <kacperkonradkowalczyk@gmail.com>
2025-10-03 19:51:27 +0200

    Added routes for adding email and changing username

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,6 +5,8 @@ import loadUser from '../middlewares/loadUser.js';
 import {
     changePassword,
     removeEmail,
+    addEmail,
+    changeUsername,
 } from '../controllers/usersController.js';
 
 const router = express.Router();
@@ -14,5 +16,11 @@ router.patch('/me/password', [checkBody, authToken, loadUser], changePassword);
 
 // remove user e-mail
 router.delete('/me/email', [authToken, loadUser], removeEmail);
+
+// adding email
+router.patch('/me/email', [checkBody, authToken, loadUser], addEmail);
+
+// change username
+router.patch('/me/username', [checkBody, authToken, loadUser], changeUsername);
 
 export default router;
